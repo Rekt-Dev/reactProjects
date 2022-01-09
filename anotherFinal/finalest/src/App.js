49dcbceca76469bdb238d561c74b28d33950963f
rektDev <rektdevvv@gmail.com>
2022-01-09 12:09:57 +0200

    this was commited through my autoScript

--- anotherFinal/finalest/src/App.js
+++ anotherFinal/finalest/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import React from "react"
 import Login from "./components/Login"
-let isLoggedIn = false
+let isLoggedIn = true
+let greeting=""
+const timeNow = new Date().getHours()
 
+console.log(`this is the time now ${timeNow}`)
 
-
+function getTime(){
+if(timeNow<12){
+   greeting="Gmornin 2 ya"
+} else if (timeNow<18){
+   greeting="Gdafternoon 2 ya"
+} else { greeting="Gdevenin 2 ya"}
+}
+getTime()
 
 function App() {
-  return (
+  return ( 
     <div className='container'>
-      {isLoggedIn===true ? <h1>Sup sire/maam ? U r logged in. </h1>:<Login />}
+      {isLoggedIn===true ? 
+           <div>
+             
+             <h1>Sup sire/maam ? U r logged in. </h1>
+              <h1>{greeting}</h1>
 
-    </div>)
+           </div>:<Login />}
+
+    </div>) 
 
   }
 export default App;
