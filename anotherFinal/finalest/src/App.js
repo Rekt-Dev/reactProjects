118878c0ec14b4db15661e198cbf34490a15cfa0
rektDev <rektdevvv@gmail.com>
2022-01-09 13:27:30 +0200

    this was commited through my autoScript

--- anotherFinal/finalest/src/App.js
+++ anotherFinal/finalest/src/App.js
@@ -18,14 +18,35 @@ getTime()
 
 function App() {
   return ( 
-    <div className='container'>
+    <div className='container'> 
       {isLoggedIn===true ? 
            <div>
              
-             <h1>Sup sire/maam ? U r logged in. </h1>
-              <h1>{greeting}</h1>
+             <h1>Sup sire/maam ?</h1>
+<br />
+               <h1> U r logged in. </h1>
+              <h1 className=''>{greeting}</h1>
+              <br />
+              
+              <h1>This is your personal page</h1>
+              <h3>here, you can edit your tasks, reminders, contacts & calendar, setup meetings, automatic call remiders and much much more. </h3>
+              <br />
+              <button id = "portfolio" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">calendar</button>
+              <button id = "portfolio" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">reminders</button>
+              <button id = "portfolio" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">tasks</button>
+              <button id = "portfolio" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">skateboarding</button>
+              <button id = "portfolio" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">weather</button>
+              <button id = "portfolio" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">world clock</button>
+            
+              <button id = "portfolio"className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">my portfolio</button>
+<br></br>
+<br></br>
 
-           </div>:<Login />}
+<br></br>
+<div>Footer gonna b here sire.</div>
+
+           </div>
+          : <Login />}
 
     </div>) 
 
