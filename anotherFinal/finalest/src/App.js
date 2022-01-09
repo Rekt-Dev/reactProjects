df32df94b7b12851f9da57b7dcb55cc276b9b75c
rektDev <rektdevvv@gmail.com>
2022-01-09 08:53:05 +0200

    this was commited through my autoScript

--- anotherFinal/finalest/src/App.js
+++ anotherFinal/finalest/src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
+
+
+let isLoggedIn = false
+
+function conditionRendering(){ 
+  if (isLoggedIn===true){
+    return <h1>Sup sire/maam ? <br></br>
+     <br></br>
+      U r logged in.</h1>
+      
+  } else {
+    return (<form className='form' >
+    <input type='text' placeholder="Username" />
+  <input type= 'password' placeholder='Password' />
+  <button type="submit">Login</button> 
+</form>)
+  }
+}
+
 
 function App() {
   return (
-    <div >
-      
+    <div className='container' >
+      <h1>Hello ! </h1>
+      {conditionRendering()}
     </div>
   );
 }
