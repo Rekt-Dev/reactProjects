adc0b3e763e6be1c64a76b8aeb66918e1520bcd5
rektDev <rektdevvv@gmail.com>
2022-01-11 22:45:06 +0200

    this was commited through my autoScript

--- State/Counter/counter/src/App.js
+++ State/Counter/counter/src/App.js
@@ -6,20 +6,17 @@ import React, {useState} from "react"
 function App() {
 
   function Increase(){
-    console.log(count)
     setCount(count+1)  
     return count
   }
   
   function Decrease(){
-    console.log(count)
 setCount(count-1)
    return count
   }
   const [count, setCount] = useState(0)
 
-  console.log(`this is usest8: ${useState}`)
-  console.log(`this is st8 ${count}`)
+
 
   return (
     <div className="div">
